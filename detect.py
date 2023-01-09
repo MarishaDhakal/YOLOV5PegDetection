664ffa5e87a1fda9705c303f7f6a19b429173587
MarishaDhakal <64780460+MarishaDhakal@users.noreply.github.com>
2023-01-08 22:59:11 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -232,7 +232,7 @@ def run(
     #drive.mount('/drive')
     #dfpixel.to_csv('C:\\Users\maris\OneDrive\Pictures\Research work\alldata.csv')
     #dfpixel.to_csv('/content/drive/My Drive/alldata.csv')
-    dfpixel.to_csv('/content/yolov5/testfile1.csv')
+    dfpixel.to_csv('/content/YOLOV5PegDetection/testfile1.csv')
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
